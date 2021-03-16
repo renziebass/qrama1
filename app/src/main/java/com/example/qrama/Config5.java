f062d75ad5ed0ad30f5b211522401263d93160d4
renzie <renziebassig@gmail.com>
2021-03-16 23:02:01 +0800

    php to textview

--- app/src/main/java/com/example/qrama/Config5.java
+++
@@ -1,8 +0,0 @@
-package com.example.qrama;
-
-public class Config5 {
-    public static final String DATA_URL = "http://192.168.254.194/qrama/student_main.php";
-    public static final String KEY_FIRSTNAME = "first_name";
-    public static final String DEPARTMENT = "department";
-    public static final String JSON_ARRAY = "result";
-}
--- app/src/main/java/com/example/qrama/student_main.java
+++ app/src/main/java/com/example/qrama/student_main.java
@@ -3,77 +3,90 @@ package com.example.qrama;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.TableLayout;
 import android.widget.TextView;
-
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.Toast;
-
-import com.android.volley.RequestQueue;
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 
 
 public class student_main extends AppCompatActivity {
 
+    Button AttendClassButton;
     TextView student_active_id;
-    TextView student_active_fullname = null;
-    TextView student_active_course = null;
-
+    TextView phpfullname;
+    TextView phpcourse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
+
+        phpcourse = findViewById(R.id.student_active_course);
+        phpfullname = findViewById(R.id.student_active_fullname);
+
+        AttendClassButton = findViewById(R.id.AttendClassButton);
+        AttendClassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(student_main.this, student_camera.class);
+                startActivity(intent);
+            }
+        });
         student_active_id = findViewById(R.id.student_active_id);
         SharedPreferences sharedPreferences = getSharedPreferences("myKey", MODE_PRIVATE);
         String ASid = sharedPreferences.getString("value", "");
         student_active_id.setText(ASid);
-        getData();
+
+        getSqlDetails();
+
+    }
+    private void getSqlDetails() {
+        SharedPreferences sharedPreferences = getSharedPreferences("myKey", MODE_PRIVATE);
+        String ASid = sharedPreferences.getString("value", "");
+
+        String url= "http://192.168.254.194/QRAMA/student_main.php?id="+ASid;
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONArray jsonarray = new JSONArray(response);
+                            for(int i=0; i < jsonarray.length(); i++) {
+                                JSONObject jsonobject = jsonarray.getJSONObject(i);
+
+                                String name = jsonobject.getString("first_name");
+                                String department = jsonobject.getString("department");
+                                phpfullname.setText(name);
+                                phpcourse.setText(department);
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if(error != null){
+                            Toast.makeText(getApplicationContext(), "Something went wrong.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
     }
 
-    private void getData() {
-
-        String url = Config5.DATA_URL + student_active_id.getText().toString().trim();
-
-        StringRequest stringRequest = new StringRequest(url, response -> showJSON(response), error -> Toast.makeText(student_main.this, error.getMessage(), Toast.LENGTH_LONG).show());
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
-    }
-    private void showJSON(String response) {
-
-        try {
-            JSONObject jsonObject = new JSONObject(response);
-            JSONArray result = jsonObject.getJSONArray(Config5.JSON_ARRAY);
-            for (int i = 0; i < result.length(); i++) {
-                JSONObject jo = result.getJSONObject(i);
-                String first_name = jo.getString(Config5.KEY_FIRSTNAME);
-                String department = jo.getString(Config5.DEPARTMENT);
-                student_active_fullname.setText(first_name);
-                student_active_course.setText(department);
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+}
 
 
-    }}
